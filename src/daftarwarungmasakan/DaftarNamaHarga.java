2c8862e643f61f665db4fea0b8034429a53daf6c
novakumalasari <novakmlsri3488@gmail.com>
2021-06-20 11:22:33 +0700

    Class 2

---
+++ src/daftarwarungmasakan/DaftarNamaHarga.java
@@ -0,0 +1,18 @@
+package daftarwarungmasakan;
+
+import model.WarungMasakanBanjar;
+
+public class DaftarNamaHarga extends WarungMasakanBanjar {
+    public String bayar, total, bonus, kembalian;
+
+    public void bns () {
+        System.out.println("Dikarenakan belanja diatas Rp. 30.000 anda mendapatkan FREE  : "+bonus );
+
+    }
+    public void akhir () {
+        System.out.println("Total Harga : "+total);
+        System.out.println("Bayar : "+bayar);
+        System.out.println("Kembalian : "+kembalian);
+    }
+
+}
